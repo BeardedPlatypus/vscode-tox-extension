a5ac5135f41db38aa82c784bb6ec3bdcfb90d8d5
BeardedPlatypus <mwtegelaers@gmail.com>
2022-12-27 18:08:13 -0500

    Add structure App.fs

--- core/src/App.fs
+++ core/src/App.fs
@@ -1,6 +1,9 @@
 module Lib
 
 
+open System
+
+
 type public ToxTask = {
     full_env_name: string;
     line: int;
@@ -14,14 +17,28 @@ type public ToxStructure = {
 }
 
 
-let public parseToxStructure (name: string) (tox_string: string): ToxStructure =
+let private retrieveTestEnvs (lines: string list): ToxTask list = 
+    []
+
+
+let private retrieveEnvListElements (lines: string list): ToxTask list =
+    []
+
+
+let private buildToxStructure (name: string) (tasks: ToxTask list): ToxStructure =
     let sub_tests: ToxTask list = []
     let sub_structures: ToxStructure list = []
 
-    let root_structure = {
-        name = name;
-        sub_structures = sub_structures;
-        sub_tests = sub_tests;
+    { name = name;
+      sub_structures = sub_structures;
+      sub_tests = sub_tests;
     }
 
-    root_structure
+
+let public parseToxStructure (name: string) (tox_string: string): ToxStructure =
+    let lines: string list = 
+        tox_string.Split([|'\n'|],  StringSplitOptions.RemoveEmptyEntries)
+        |> Array.toList
+    
+    let tasks = Set((retrieveTestEnvs lines) @ (retrieveEnvListElements lines)) |> Set.toList
+    buildToxStructure name tasks
