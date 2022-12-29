f3e9e1944d1913a88d59f57cf84d31d6a80ba29a
BeardedPlatypus <mwtegelaers@gmail.com>
2022-12-29 11:12:37 -0500

    Add initial envlist reading

--- core/src/App.fs
+++ core/src/App.fs
@@ -5,15 +5,27 @@ open System
 
 
 type public ToxTask = {
-    full_env_name: string;
-    line: int;
+    full_env_name: string
+    pretty_name: string
+    line: int
 }
+
+
+let private toToxTask (line: int) (full_name: string): ToxTask list = 
+    // TODO: support factors
+    // TODO: support nesting
+    [ {
+        full_env_name = full_name
+        pretty_name = full_name
+        line = line
+      }
+    ]
 
 
 type public ToxStructure = {
     name: string
-    sub_structures: ToxStructure list;
-    sub_tests: ToxTask list;
+    sub_structures: ToxStructure array
+    sub_tests: ToxTask array
 }
 
 
@@ -21,17 +33,68 @@ let private retrieveTestEnvs (lines: string list): ToxTask list =
     []
 
 
+type private EnvListAcc = {
+    line: int
+    isInToxEnv: bool
+    isParsing: bool
+    isFinished: bool
+    elements: ToxTask list
+}
+
+
+let private defaultEnvListAcc: EnvListAcc = {
+    line = 0
+    isInToxEnv = false
+    isParsing = false
+    isFinished = false
+    elements = []
+}
+
+
+let private isEnvListDeclaration (line: string): bool =
+    ["env_list ="; "env_list="; "envlist ="; "envlist="]
+    |> List.exists line.StartsWith
+
+
+let private retrieveEnvFromEnvlistLine (line: string) (line_number: int): ToxTask list =
+    line.Split([|' '; ','|], StringSplitOptions.RemoveEmptyEntries) 
+    |> Array.toList
+    |> List.collect (toToxTask line_number)
+
+
+let private envListFold (acc: EnvListAcc) (line: string): EnvListAcc = 
+    let toxSectionName = "[tox]"  // This currently does not take into account setup.cfg, or pyproject.toml
+    let trimmedLine = line.Trim()
+     
+    let updatedAcc =
+        if acc.isFinished then
+            acc
+        elif not acc.isInToxEnv && trimmedLine.Equals(toxSectionName) then
+            { acc with isInToxEnv = true }
+        elif acc.isInToxEnv && trimmedLine.StartsWith("[") && trimmedLine.Contains("]") then
+            { acc with isInToxEnv = false; isFinished = true }
+        elif acc.isInToxEnv && isEnvListDeclaration(trimmedLine) then
+            let value = line.Split([|'='|], 2)[1]
+            let new_elements = retrieveEnvFromEnvlistLine value acc.line
+            { acc with isParsing = true; elements = acc.elements @ new_elements}
+        else
+            acc
+
+    { updatedAcc with line = acc.line + 1 }
+
+
 let private retrieveEnvListElements (lines: string list): ToxTask list =
-    []
+    let result = List.fold envListFold defaultEnvListAcc lines
+    result.elements
 
 
 let private buildToxStructure (name: string) (tasks: ToxTask list): ToxStructure =
-    let sub_tests: ToxTask list = []
+    // let sub_tests: ToxTask list = []
     let sub_structures: ToxStructure list = []
 
     { name = name;
-      sub_structures = sub_structures;
-      sub_tests = sub_tests;
+      sub_structures = sub_structures |> List.toArray;
+      sub_tests = tasks |> List.toArray;
     }
 
 
--- extension/src/test/suite/extension.test.ts
+++ extension/src/test/suite/extension.test.ts
@@ -4,12 +4,41 @@ import * as assert from 'assert';
 // as well as import your extension to test it
 import * as vscode from 'vscode';
 // import * as myExtension from '../../extension';
+let core = require("../../core.js");
+
+
 
 suite('Extension Test Suite', () => {
 	vscode.window.showInformationMessage('Start all tests.');
 
-	test('Sample test', () => {
-		assert.strictEqual(-1, [1, 2, 3].indexOf(5));
-		assert.strictEqual(-1, [1, 2, 3].indexOf(0));
+	const envlist_without_factors = `
+[tox]
+min_version = 4.0
+env_list = py310, py39, type
+
+[testenv]
+deps = pytest
+commands = pytest tests
+`
+
+	test('envlist without factors parses correctly', () => {
+		const name = "tox.ini"
+		var rootStructure = core.ToxParserLib.parseToxStructure(
+			name, envlist_without_factors
+		);
+
+		assert.strictEqual(rootStructure.name, name)
+		assert.strictEqual(rootStructure.sub_structures.length, 0)
+		assert.strictEqual(rootStructure.sub_tests.length, 3)
+
+		const assertSameTask = (task: any, expected_name: string) => {
+			assert.strictEqual(task.full_env_name, expected_name)
+			assert.strictEqual(task.line, 2)
+			assert.strictEqual(task.pretty_name, expected_name)
+		}
+
+		assertSameTask(rootStructure.sub_tests[0], "py310")
+		assertSameTask(rootStructure.sub_tests[1], "py39")
+		assertSameTask(rootStructure.sub_tests[2], "type")
 	});
 });
