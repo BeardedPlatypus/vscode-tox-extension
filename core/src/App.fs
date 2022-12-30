e416acd8e97bcc05bb6480318b04fad8893e9445
BeardedPlatypus <mwtegelaers@gmail.com>
2022-12-30 18:30:28 -0500

    Add support for factors

--- core/src/App.fs
+++ core/src/App.fs
@@ -11,15 +11,56 @@ type public ToxTask = {
 }
 
 
+let private computeFactors (name: string) : (string list) list =
+    let splitConditionals (factor: string): string list =
+        // TODO: Handle the case where a factor is malformed.
+        if factor.Contains "{" && factor.Contains "}" then
+            let components = factor.Split([|'{'; '}'|])
+            let prefix = components.[0]
+            let conditionals = components.[1]
+            let postfix = components.[2]
+
+            conditionals.Split([|','|])
+            |> Array.toList
+            |> List.map (fun (c: string) -> $"{prefix}{c}{postfix}")
+        else
+            [ factor ]
+            
+
+    let factors = name.Split([|'-'|], StringSplitOptions.RemoveEmptyEntries)
+
+    factors 
+    |> Array.toList
+    |> List.map splitConditionals
+
+
+let private generateToxNames (factors: (string list) list): string list =
+    match factors with
+    | head :: tail ->
+        let addFactor (factor: string) =
+            List.map (fun (s: string) -> $"{s}-{factor}")
+        
+        let folder (acc: string list) (v: string list) =
+            List.collect (fun v_elem -> addFactor v_elem acc) v
+        List.fold folder head tail
+    | [] -> 
+        []
+
+
 let private toToxTask (line: int) (full_name: string): ToxTask list = 
-    // TODO: support factors
-    // TODO: support nesting
-    [ {
-        full_env_name = full_name
-        pretty_name = full_name
+    // Note we currently assume the name is well-formed. This includes the following assumptions:
+    // * '-' does not occur in conditional statements, i.e. inside '{.*}'.
+    // * Factors contain at most one '{.*}' substring.
+    // * If a factor contains a '{' then it will contain a '}' and vice-versa.
+    let toTask (task: string): ToxTask = {
+        full_env_name = task 
+        pretty_name = task
         line = line
-      }
-    ]
+    }
+
+    computeFactors full_name
+    |> generateToxNames
+    |> List.map toTask
 
 
 type public ToxStructure = {
@@ -56,9 +97,57 @@ let private isEnvListDeclaration (line: string): bool =
     |> List.exists line.StartsWith
 
 
+type private SplitTasksAcc = {
+    current_element_reversed: char list
+    is_in_factor: bool
+    elements_reversed: string list
+}
+
+
+let private defaultSplitTasksAcc: SplitTasksAcc = {
+    current_element_reversed = []
+    is_in_factor = false
+    elements_reversed = []
+}
+
+
+let private toElement (l: char list): string =
+    String.Join("", l |> List.rev)
+
+
+let private SplitTasksFolder (acc: SplitTasksAcc) (v: char): SplitTasksAcc = 
+    match v with
+    // Spaces are trimmed by default to simplify parsing later on
+    | ' ' -> acc
+    // We assume that we cannot have nested factors.
+    | '{' -> 
+        { acc with current_element_reversed = '{' :: acc.current_element_reversed; 
+                   is_in_factor = true
+        } 
+    | '}' -> 
+        { acc with current_element_reversed = '}' :: acc.current_element_reversed; 
+                   is_in_factor = false
+        }
+    | ',' when not acc.is_in_factor -> 
+        let current_element = acc.current_element_reversed |> toElement
+        { acc with current_element_reversed = []
+                   elements_reversed = current_element :: acc.elements_reversed
+        }
+    | c -> 
+        { acc with current_element_reversed = c :: acc.current_element_reversed }
+
+
+let private splitTasks (line: string): string list =
+    let tasks_acc = line |> Seq.fold SplitTasksFolder defaultSplitTasksAcc
+    let last_element = tasks_acc.current_element_reversed |> toElement
+
+    last_element :: tasks_acc.elements_reversed
+    |> List.filter (fun t -> not (String.IsNullOrEmpty t))
+    |> List.rev
+
+
 let private retrieveEnvFromEnvlistLine (line: string) (line_number: int): ToxTask list =
-    line.Split([|' '; ','|], StringSplitOptions.RemoveEmptyEntries) 
-    |> Array.toList
+    splitTasks line
     |> List.collect (toToxTask line_number)
 
 
@@ -110,3 +199,4 @@ let public parseToxStructure (name: string) (tox_string: string): ToxStructure =
     
     let tasks = Set((retrieveTestEnvs lines) @ (retrieveEnvListElements lines)) |> Set.toList
     buildToxStructure name tasks
+
--- extension/src/test/suite/extension.test.ts
+++ extension/src/test/suite/extension.test.ts
@@ -77,4 +77,78 @@ commands = pytest tests
 		assertSameTask(rootStructure.sub_tests[1], "py39", 4)
 		assertSameTask(rootStructure.sub_tests[2], "type", 5)
 	});
+
+
+	const envlist_with_factors_and_multiline = `
+[tox]
+min_version = 4.0
+env_list = 
+	py39-django{15, 16, 17}
+	{py39, py310}-{potato, tomato}-test
+	type
+
+[testenv]
+deps = pytest
+commands = pytest tests
+`
+	
+    test('envlist with factors and multi lines parses correctly', () => {
+		const name = "tox.ini"
+		var rootStructure = core.ToxParserLib.parseToxStructure(
+			name, envlist_with_factors_and_multiline
+		);
+
+		assert.strictEqual(rootStructure.name, name)
+		assert.strictEqual(rootStructure.sub_structures.length, 0)
+		assert.strictEqual(rootStructure.sub_tests.length, 8)
+
+		const assertSameTask = (task: any, expected_name: string, line: number) => {
+			assert.strictEqual(task.full_env_name, expected_name)
+			assert.strictEqual(task.line, line)
+			assert.strictEqual(task.pretty_name, expected_name)
+		}
+		assertSameTask(rootStructure.sub_tests[0], "py310-potato-test", 4)
+		assertSameTask(rootStructure.sub_tests[1], "py310-tomato-test", 4)
+		assertSameTask(rootStructure.sub_tests[2], "py39-django15", 3)
+		assertSameTask(rootStructure.sub_tests[3], "py39-django16", 3)
+		assertSameTask(rootStructure.sub_tests[4], "py39-django17", 3)
+		assertSameTask(rootStructure.sub_tests[5], "py39-potato-test", 4)
+		assertSameTask(rootStructure.sub_tests[6], "py39-tomato-test", 4)
+		assertSameTask(rootStructure.sub_tests[7], "type", 5)
+	});
+
+	const envlist_with_factors= `
+[tox]
+min_version = 4.0
+env_list = py39-django{15, 16, 17}, {py39, py310}-{potato, tomato}-test, type
+
+[testenv]
+deps = pytest
+commands = pytest tests
+`
+	
+    test('envlist with factors parses correctly', () => {
+		const name = "tox.ini"
+		var rootStructure = core.ToxParserLib.parseToxStructure(
+			name, envlist_with_factors
+		);
+
+		assert.strictEqual(rootStructure.name, name)
+		assert.strictEqual(rootStructure.sub_structures.length, 0)
+		assert.strictEqual(rootStructure.sub_tests.length, 8)
+
+		const assertSameTask = (task: any, expected_name: string, line: number) => {
+			assert.strictEqual(task.full_env_name, expected_name)
+			assert.strictEqual(task.line, line)
+			assert.strictEqual(task.pretty_name, expected_name)
+		}
+		assertSameTask(rootStructure.sub_tests[0], "py310-potato-test", 2)
+		assertSameTask(rootStructure.sub_tests[1], "py310-tomato-test", 2)
+		assertSameTask(rootStructure.sub_tests[2], "py39-django15", 2)
+		assertSameTask(rootStructure.sub_tests[3], "py39-django16", 2)
+		assertSameTask(rootStructure.sub_tests[4], "py39-django17", 2)
+		assertSameTask(rootStructure.sub_tests[5], "py39-potato-test", 2)
+		assertSameTask(rootStructure.sub_tests[6], "py39-tomato-test", 2)
+		assertSameTask(rootStructure.sub_tests[7], "type", 2)
+	});
 });
