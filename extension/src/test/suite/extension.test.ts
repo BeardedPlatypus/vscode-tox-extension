dfc7d0b18f7b9be608020c3c707ed6f4e8d96b4b
BeardedPlatypus <mwtegelaers@gmail.com>
2022-12-31 12:02:26 -0500

    Update tests with nesting

--- extension/src/test/suite/extension.test.ts
+++ extension/src/test/suite/extension.test.ts
@@ -28,9 +28,9 @@ commands = pytest tests
 			name, envlist_without_factors
 		);
 
-		assert.strictEqual(rootStructure.name, name)
-		assert.strictEqual(rootStructure.sub_structures.length, 0)
-		assert.strictEqual(rootStructure.sub_tests.length, 3)
+		assert.strictEqual(rootStructure.name, name);
+		assert.strictEqual(rootStructure.sub_structures.length, 0);
+		assert.strictEqual(rootStructure.sub_tests.length, 3);
 
 		const assertSameTask = (task: any, expected_name: string) => {
 			assert.strictEqual(task.full_env_name, expected_name)
@@ -38,9 +38,9 @@ commands = pytest tests
 			assert.strictEqual(task.pretty_name, expected_name)
 		}
 
-		assertSameTask(rootStructure.sub_tests[0], "py310")
-		assertSameTask(rootStructure.sub_tests[1], "py39")
-		assertSameTask(rootStructure.sub_tests[2], "type")
+		assertSameTask(rootStructure.sub_tests[0], "py310");
+		assertSameTask(rootStructure.sub_tests[1], "py39");
+		assertSameTask(rootStructure.sub_tests[2], "type");
 	});
 
 	// TODO: Verify that this works in tox with commas in multiple lines.
@@ -63,19 +63,19 @@ commands = pytest tests
 			name, envlist_with_multiple_lines
 		);
 
-		assert.strictEqual(rootStructure.name, name)
-		assert.strictEqual(rootStructure.sub_structures.length, 0)
-		assert.strictEqual(rootStructure.sub_tests.length, 3)
+		assert.strictEqual(rootStructure.name, name);
+		assert.strictEqual(rootStructure.sub_structures.length, 0);
+		assert.strictEqual(rootStructure.sub_tests.length, 3);
 
 		const assertSameTask = (task: any, expected_name: string, line: number) => {
-			assert.strictEqual(task.full_env_name, expected_name)
-			assert.strictEqual(task.line, line)
-			assert.strictEqual(task.pretty_name, expected_name)
+			assert.strictEqual(task.full_env_name, expected_name);
+			assert.strictEqual(task.line, line);
+			assert.strictEqual(task.pretty_name, expected_name);
 		}
 
-		assertSameTask(rootStructure.sub_tests[0], "py310", 3)
-		assertSameTask(rootStructure.sub_tests[1], "py39", 4)
-		assertSameTask(rootStructure.sub_tests[2], "type", 5)
+		assertSameTask(rootStructure.sub_tests[0], "py310", 3);
+		assertSameTask(rootStructure.sub_tests[1], "py39", 4);
+		assertSameTask(rootStructure.sub_tests[2], "type", 5);
 	});
 
 
@@ -98,23 +98,54 @@ commands = pytest tests
 			name, envlist_with_factors_and_multiline
 		);
 
-		assert.strictEqual(rootStructure.name, name)
-		assert.strictEqual(rootStructure.sub_structures.length, 0)
-		assert.strictEqual(rootStructure.sub_tests.length, 8)
+		assert.strictEqual(rootStructure.name, name);
+		assert.strictEqual(rootStructure.sub_structures.length, 2);
+		assert.strictEqual(rootStructure.sub_tests.length, 1);
 
-		const assertSameTask = (task: any, expected_name: string, line: number) => {
-			assert.strictEqual(task.full_env_name, expected_name)
-			assert.strictEqual(task.line, line)
-			assert.strictEqual(task.pretty_name, expected_name)
+		const assertSameTask = (task: any, expected_name: string, line: number, pretty_name: string) => {
+			assert.strictEqual(task.full_env_name, expected_name);
+			assert.strictEqual(task.line, line);
+			assert.strictEqual(task.pretty_name, pretty_name);
 		}
-		assertSameTask(rootStructure.sub_tests[0], "py310-potato-test", 4)
-		assertSameTask(rootStructure.sub_tests[1], "py310-tomato-test", 4)
-		assertSameTask(rootStructure.sub_tests[2], "py39-django15", 3)
-		assertSameTask(rootStructure.sub_tests[3], "py39-django16", 3)
-		assertSameTask(rootStructure.sub_tests[4], "py39-django17", 3)
-		assertSameTask(rootStructure.sub_tests[5], "py39-potato-test", 4)
-		assertSameTask(rootStructure.sub_tests[6], "py39-tomato-test", 4)
-		assertSameTask(rootStructure.sub_tests[7], "type", 5)
+
+		assertSameTask(rootStructure.sub_tests[0], "type", 5, "type")
+
+		const assertSameStructure = (sub_structure: any, expected_name: string, expected_tests: [string, number, string][]) => {
+			assert.strictEqual(sub_structure.name, expected_name);
+			assert.strictEqual(sub_structure.sub_structures.length, 0);
+			
+			assert.strictEqual(sub_structure.sub_tests.length, expected_tests.length);
+
+			for (var i=0; i < sub_structure.sub_tests.length; i++) {
+				assertSameTask(
+					sub_structure.sub_tests[i], 
+					expected_tests[i][0],
+					expected_tests[i][1],
+					expected_tests[i][2]
+				);
+			}
+		}
+
+		assertSameStructure(
+			rootStructure.sub_structures[0],
+			"py310",
+			[
+				["py310-potato-test", 4, "potato-test"],
+				["py310-tomato-test", 4, "tomato-test"],
+			]
+		);
+
+		assertSameStructure(
+			rootStructure.sub_structures[1],
+			"py39",
+			[
+				["py39-django15", 3, "django15"],
+		        ["py39-django16", 3, "django16"],
+		        ["py39-django17", 3, "django17"],
+		        ["py39-potato-test", 4, "potato-test"],
+		        ["py39-tomato-test", 4, "tomato-test"],
+			]
+		);
 	});
 
 	const envlist_with_factors= `
@@ -133,22 +164,53 @@ commands = pytest tests
 			name, envlist_with_factors
 		);
 
-		assert.strictEqual(rootStructure.name, name)
-		assert.strictEqual(rootStructure.sub_structures.length, 0)
-		assert.strictEqual(rootStructure.sub_tests.length, 8)
+		assert.strictEqual(rootStructure.name, name);
+		assert.strictEqual(rootStructure.sub_structures.length, 2);
+		assert.strictEqual(rootStructure.sub_tests.length, 1);
 
-		const assertSameTask = (task: any, expected_name: string, line: number) => {
-			assert.strictEqual(task.full_env_name, expected_name)
-			assert.strictEqual(task.line, line)
-			assert.strictEqual(task.pretty_name, expected_name)
+		const assertSameTask = (task: any, expected_name: string, line: number, pretty_name: string) => {
+			assert.strictEqual(task.full_env_name, expected_name);
+			assert.strictEqual(task.line, line);
+			assert.strictEqual(task.pretty_name, pretty_name);
 		}
-		assertSameTask(rootStructure.sub_tests[0], "py310-potato-test", 2)
-		assertSameTask(rootStructure.sub_tests[1], "py310-tomato-test", 2)
-		assertSameTask(rootStructure.sub_tests[2], "py39-django15", 2)
-		assertSameTask(rootStructure.sub_tests[3], "py39-django16", 2)
-		assertSameTask(rootStructure.sub_tests[4], "py39-django17", 2)
-		assertSameTask(rootStructure.sub_tests[5], "py39-potato-test", 2)
-		assertSameTask(rootStructure.sub_tests[6], "py39-tomato-test", 2)
-		assertSameTask(rootStructure.sub_tests[7], "type", 2)
+
+		assertSameTask(rootStructure.sub_tests[0], "type", 5, "type")
+
+		const assertSameStructure = (sub_structure: any, expected_name: string, expected_tests: [string, number, string][]) => {
+			assert.strictEqual(sub_structure.name, expected_name);
+			assert.strictEqual(sub_structure.sub_structures.length, 0);
+			
+			assert.strictEqual(sub_structure.sub_tests.length, expected_tests.length);
+
+			for (var i=0; i < sub_structure.sub_tests.length; i++) {
+				assertSameTask(
+					sub_structure.sub_tests[i], 
+					expected_tests[i][0],
+					expected_tests[i][1],
+					expected_tests[i][2]
+				);
+			}
+		}
+
+		assertSameStructure(
+			rootStructure.sub_structures[0],
+			"py310",
+			[
+				["py310-potato-test", 2, "potato-test"],
+				["py310-tomato-test", 2, "tomato-test"],
+			]
+		);
+
+		assertSameStructure(
+			rootStructure.sub_structures[1],
+			"py39",
+			[
+				["py39-django15", 2, "django15"],
+		        ["py39-django16", 2, "django16"],
+		        ["py39-django17", 2, "django17"],
+		        ["py39-potato-test", 2, "potato-test"],
+		        ["py39-tomato-test", 2, "tomato-test"],
+			]
+		);
 	});
 });
